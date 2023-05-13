ced4cabfc5144dfe6ca1a92448ca58b2ac772cab
RenWolfstein <mbimam03@gmail.com>
2023-05-13 13:51:19 +0800

    Percabangan Switch

---
+++ src/main/java/Percabangan/PercabanganSwitch.java
@@ -0,0 +1,24 @@
+package Percabangan;
+public class PercabanganSwitch {
+    public static void main(String[] args){
+       int jalanjalan = 3;
+       switch(jalanjalan){
+           case 1: 
+               System.out.println("Urang Lapas");
+               break;
+           case 2:
+               System.out.println("Urang Acayaan");
+               break;
+           case 3: 
+               System.out.println("Ubat Nyamuk ");
+               break;
+           case 4: 
+               System.out.println("dubil dit");
+               break;
+           default :
+               System.out.println("Mahluk halus");
+               break;
+       }
+       
+   }    
+}
