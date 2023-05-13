1e628ad136fea60c05a68c0c49f6c1a297d34648
RenWolfstein <mbimam03@gmail.com>
2023-05-13 12:40:39 +0800

    Operasi Binary Aritmatika

--- src/main/java/Operator/OperatorBinary.java
+++ src/main/java/Operator/OperatorBinary.java
@@ -3,4 +3,13 @@ public class OperatorBinary {
     String nama = "M. Rizki Bima Mubarak";
     int jumlahSks = 24;
     double iPK = 3.87;
+    
+    public static void main(String[] args) {
+        System.out.println(9+3);
+        System.out.println(9-3);
+        System.out.println(9*3);
+        System.out.println(9/3);
+        System.out.println(9%3);
+        System.out.println(9%2);
+    }
 }
