c513f8decb50685ab1d224b1650246f9e8df0801
RenWolfstein <mbimam03@gmail.com>
2023-05-13 11:46:00 +0800

    Buat Operasi String

---
+++ src/main/java/pertemuan03/String/Pbo1Pertemuan03.java
@@ -0,0 +1,9 @@
+package pertemuan03.String;
+public class Pbo1Pertemuan03 {
+
+    public static void main(String[] args) {
+        String kota = "Yogyakarta";
+        
+        System.out.println(kota);
+    }
+}
