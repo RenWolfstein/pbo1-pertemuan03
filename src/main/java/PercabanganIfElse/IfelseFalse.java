d357047cb378e0c224a33f8aecbc0282053dd93e
RenWolfstein <mbimam03@gmail.com>
2023-05-13 13:43:47 +0800

    IfElseFalse
    Percabangan If else dengan condition false sehingga statement 2 jalan

---
+++ src/main/java/PercabanganIfElse/IfelseFalse.java
@@ -0,0 +1,14 @@
+package PercabanganIfElse;
+//If dengan condition false sehingga statement 2 jalan
+public class IfelseFalse {
+    public static void main(String[] args) {
+        double tBelanja = 53000;
+        double tUang = 55000;
+        
+        if(tUang<tBelanja){
+            System.out.println("Uang kurang, kurangi belanjaan anda"); //statement 1
+        }else{
+            System.out.println("Uang cukup, Happy Shopping"); //Statement 2
+        }
+    }
+}
